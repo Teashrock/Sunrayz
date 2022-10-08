edb1cb861fc81a59ff3a63d1e0ec24fb4fc14a06
Teashrock <kajitsu22@gmail.com>
2022-10-08 19:47:35 +0300

    Added dependencies information

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # Sunrayz
 Sunrayz is a visual novel development engine, inspired by Ren'Py, Light.vn and Novelty.
 
-### Building
+# Dependencies
+### Windows
+- MinGW GCC 8.3.0+ (Visual Studio is not supported)
+- Python 3.10.6 (may work on a lower version, but not tested)
+- Windows versions of ``unix2dos`` and ``patch`` (both can be found [here](https://gnuwin32.sourceforge.net/packages.html))
+- Also please double make sure that all utilities above are present in PATH.
+
+### Linux, Haiku
+- GCC 8.3.0+
+- Python 3.10.6 (may work on a lower version, but not tested)
+- GNU Patch
+
+# Building
 To build, enter the following in your terminal:
 ```
 cd Sunrayz
--- src/fonts/fonts.h
+++ src/fonts/fonts.h
@@ -34,3 +34,35 @@ unsigned long GetFontLibertinusSerif_SemiboldSize();
 // LibertinusSerif_SemiboldItalic.c
 unsigned char* GetFontLibertinusSerif_SemiboldItalic();
 unsigned long GetFontLibertinusSerif_SemiboldItalicSize();
+
+// Calibri.c
+unsigned char* GetFontCalibri();
+unsigned long GetFontCalibriSize();
+
+// KurintoText_Rg.c
+unsigned char* GetFontKurintoText_Rg();
+unsigned long GetFontKurintoText_RgSize();
+
+// LibertinusSerif_Bold.c
+unsigned char* GetFontLibertinusSerif_Bold();
+unsigned long GetFontLibertinusSerif_BoldSize();
+
+// LibertinusSerif_BoldItalic.c
+unsigned char* GetFontLibertinusSerif_BoldItalic();
+unsigned long GetFontLibertinusSerif_BoldItalicSize();
+
+// LibertinusSerif_Italic.c
+unsigned char* GetFontLibertinusSerif_Italic();
+unsigned long GetFontLibertinusSerif_ItalicSize();
+
+// LibertinusSerif_Regular.c
+unsigned char* GetFontLibertinusSerif_Regular();
+unsigned long GetFontLibertinusSerif_RegularSize();
+
+// LibertinusSerif_Semibold.c
+unsigned char* GetFontLibertinusSerif_Semibold();
+unsigned long GetFontLibertinusSerif_SemiboldSize();
+
+// LibertinusSerif_SemiboldItalic.c
+unsigned char* GetFontLibertinusSerif_SemiboldItalic();
+unsigned long GetFontLibertinusSerif_SemiboldItalicSize();

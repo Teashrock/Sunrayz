f7e44e84c0aa8c2a77207c230eeb8ba0042a9d68
Teashrock <kajitsu22@gmail.com>
2022-10-08 13:43:52 +0300

    Fixed the stupidiest memory bug ever (seriously, what did I smoke to write such a thing?)

--- src/fonts/fonts.h
+++ src/fonts/fonts.h
@@ -2,3 +2,35 @@
 
 // This file only links static fonts with the engine.
 // Do not touch with bare hands.
+
+// Calibri.c
+unsigned char* GetFontCalibri();
+unsigned long GetFontCalibriSize();
+
+// KurintoText_Rg.c
+unsigned char* GetFontKurintoText_Rg();
+unsigned long GetFontKurintoText_RgSize();
+
+// LibertinusSerif_Bold.c
+unsigned char* GetFontLibertinusSerif_Bold();
+unsigned long GetFontLibertinusSerif_BoldSize();
+
+// LibertinusSerif_BoldItalic.c
+unsigned char* GetFontLibertinusSerif_BoldItalic();
+unsigned long GetFontLibertinusSerif_BoldItalicSize();
+
+// LibertinusSerif_Italic.c
+unsigned char* GetFontLibertinusSerif_Italic();
+unsigned long GetFontLibertinusSerif_ItalicSize();
+
+// LibertinusSerif_Regular.c
+unsigned char* GetFontLibertinusSerif_Regular();
+unsigned long GetFontLibertinusSerif_RegularSize();
+
+// LibertinusSerif_Semibold.c
+unsigned char* GetFontLibertinusSerif_Semibold();
+unsigned long GetFontLibertinusSerif_SemiboldSize();
+
+// LibertinusSerif_SemiboldItalic.c
+unsigned char* GetFontLibertinusSerif_SemiboldItalic();
+unsigned long GetFontLibertinusSerif_SemiboldItalicSize();

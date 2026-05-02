be8e5d65a410a3dad5c2861511cf203a500d08c7
Teashrock <kajitsu22@gmail.com>
2026-05-02 11:05:07 +0300

    queue.h: Filled type skeletons with some actual content

--- src/queue.h
+++ src/queue.h
@@ -1,11 +1,30 @@
 #pragma once
 
-typedef struct _SzTask {
+#include <pthread.h>
 
+typedef enum {
+    TASK_DONE,
+    TASK_NEW,
+    TASK_PREPARING,
+    TASK_READY
+} SzTaskState;
+
+typedef enum {
+    QUEUE_CLEANING,
+    QUEUE_RUNNING,
+    QUEUE_STANDBY
+} SzQueueState;
+
+typedef struct _SzTask {
+    struct _SzTask* next;
+    SzTaskState state;
 } SzTask;
 
 typedef struct _SzQueue {
-    SzTask** tasks;
+    pthread_t* thread;
+    SzQueueState state;
+    int task_limit;
+    SzTask* tasks;
 } SzQueue;
 
-int CreateQueue();
+SzQueue* CreateQueue(void);

3b1e33e2c54343c37e96aed327b06154450e3315
Teashrock <kajitsu22@gmail.com>
2026-05-02 11:06:23 +0300

    queue.c: Wrote some skeletons of the functional routine

--- src/queue.c
+++ src/queue.c
@@ -1,5 +1,39 @@
 #include "queue.h"
 
-int CreateQueue() {
-    
+#include <pthread.h>
+#include <raylib.h>
+
+SzTask* CreateTask(SzQueue* q) {
+    SzTask* ptr = q->tasks;
+    while (ptr != NULL) {
+        ptr = ptr->next;
+    }
+    ptr = (SzTask*)MemAlloc(sizeof(SzTask));
+    *ptr = (SzTask){
+        .state = TASK_NEW,
+        .next = NULL
+    };
+    q->state = QUEUE_RUNNING;
+}
+
+void* QueueRoutine(void* arg) {
+    SzQueue* q = (SzQueue*)arg;
+    SzTask* currentTask = q->tasks;
+    while (true) {
+        while (q->state == QUEUE_STANDBY); // Doing nothing while the queue is standing by
+        //currentTask
+    }
+}
+
+SzQueue* CreateQueue(void) {
+    SzQueue* ptr = (SzQueue*)MemAlloc(sizeof(SzQueue));
+
+    *ptr = (SzQueue){
+        .thread = NULL,
+        .state = QUEUE_STANDBY,
+        .tasks = NULL
+    };
+    pthread_create(ptr->thread, NULL, QueueRoutine, ptr);
+
+    return ptr;
 }

26e335fc5bd52042ee5366e7367184e7f9c95c51
Teashrock <kajitsu22@gmail.com>
2026-05-22 20:13:46 +0300

    Wrote the fundament of Reader-Task based runtime

--- src/core/runtime.h
+++ src/core/runtime.h
@@ -2,6 +2,7 @@
 
 #include <pthread.h>
 #include <stddef.h>
+#include <stdio.h>
 #include <semaphore.h>
 
 typedef enum {
@@ -9,10 +10,23 @@ typedef enum {
     TASK_WAIT
 } SzTaskState;
 
+typedef enum {
+    READER_FORWARD,
+    READER_BACKWARD
+} SzReaderMode;
+
 typedef struct _SzTask {
     int (*content)(void);
     struct _SzTask* next;
     SzTaskState state;
 } SzTask;
 
+typedef struct _SzReader {
+    pthread_t* thread;
+    sem_t movement;
+    FILE* script;
+    SzReaderMode mode;
+} SzReader;
+
 void CreateTask(int (* taskRoutine)(void));
+SzReader* CreateReader(char* script);

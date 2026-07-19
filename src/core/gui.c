422ba5fad4b1860e513b7bd0195812c75e482041
Teashrock <kajitsu22@gmail.com>
2026-07-20 01:10:37 +0300

    gui.c: Tweaked CreateConstruct

--- src/core/gui.c
+++ src/core/gui.c
@@ -3,12 +3,11 @@
 SzConstruct* CreateConstruct(SzConstruct* parent)
 {
     SzConstruct* c = (SzConstruct*)MemAlloc(sizeof(SzConstruct));
-
     *c = (SzConstruct){
         .parent = parent,
         .child = NULL,
+        .children = NULL,
         .parts = NULL
     };
-
     return c;
 }

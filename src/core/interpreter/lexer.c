bba55bd72e881e031fd11ccf608fca7d6bb1cab7
Teashrock <kajitsu22@gmail.com>
2026-08-19 14:16:13 +0300

    Added interpreter/lexer.c/h

---
+++ src/core/interpreter/lexer.c
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: BSD 3-Clause
+// Copyright (c) 2021-2026 Teashrock
+
+#include "lexer.h"
+
+char** lex() {
+    
+}
---
+++ src/core/interpreter/lexer.h
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: BSD 3-Clause
+// Copyright (c) 2021-2026 Teashrock
+
+#pragma once
